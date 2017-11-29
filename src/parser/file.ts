a4140d4871fe283589835d5608a7a32ca9286264
Jan Vlnas <git@jan.vlnas.cz>
2017-11-29 19:30:14 +0100

    Prepare file iface

--- src/parser/file.ts
+++ src/parser/file.ts
@@ -1,5 +1,11 @@
 import { IListItem } from './items'
 
+interface IListFile {
+  prefix?: string,
+  suffix?: string,
+  sections: ISection[],
+}
+
 export interface ISection {
   name: string
   depth: number
@@ -9,4 +15,6 @@ export interface ISection {
 // This assumes that parsing starts with the first h2
 const PATTERN_SEC_HEADER = /^#(#+)\s*(.*)$/
 
-export function parseFile() {}
+export function parseFile(contents: string) {
+
+}
