48d9a0e8d15609f999e330f0949b8961fc023ba8
Jan Vlnas <git@jan.vlnas.cz>
2018-07-25 14:08:42 +0200

    Items serializer

---
+++ src/serializer/__tests__/items.test.ts
@@ -0,0 +1,36 @@
+import { serializeItems } from '../items'
+
+const input = [
+  {
+    name: 'learnxinyminutes-docs',
+    url: 'https://github.com/adambard/learnxinyminutes-docs',
+    desc: 'Code documentation written as code!',
+    extras: ['https://learnxinyminutes.com/'],
+  },
+  {
+    name: 'no-free-basics',
+    url: 'https://github.com/net-neutrality/no-free-basics',
+    desc: "Those who have spoken up against Facebook's “Free Basics”",
+    extras: ['https://net-neutrality.github.io/no-free-basics/'],
+  },
+  {
+    name: 'recipes',
+    url: 'https://github.com/csclug/recipes',
+    desc: 'Delicious open source',
+    author: 'csclug',
+  },
+]
+
+describe('.serializeItems', () => {
+  test('generates Markdown from items', () => {
+    const actual = serializeItems(input)
+    const expected = `
+* [learnxinyminutes-docs](https://github.com/adambard/learnxinyminutes-docs) – Code documentation written as code!
+  - https://learnxinyminutes.com/
+* [no-free-basics](https://github.com/net-neutrality/no-free-basics) – Those who have spoken up against Facebook's “Free Basics”
+  - https://net-neutrality.github.io/no-free-basics/
+* [recipes](https://github.com/csclug/recipes) by @csclug – Delicious open source
+    `.trim()
+    expect(actual).toBe(expected)
+  })
+})
---
+++ src/serializer/items.ts
@@ -0,0 +1,18 @@
+import { ListItem } from '../types'
+
+function serializeItem(item: ListItem): string {
+  let output = `* [${item.name}](${item.url})`
+  if (item.author) {
+    output += ` by @${item.author}`
+  }
+  if (item.desc) {
+    output += ` – ${item.desc}`
+  }
+  if (item.extras) {
+    output += '\n' + item.extras.map(extra => `  - ${extra}`).join('\n')
+  }
+  return output
+}
+export function serializeItems(items: ListItem[]): string {
+  return items.map(serializeItem).join('\n')
+}
