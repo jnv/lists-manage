3ba24056fff8baf6854d19217b6e315deb5a63dc
Jan Vlnas <git@jan.vlnas.cz>
2018-07-25 14:24:39 +0200

    File serializer

---
+++ src/serializer/__tests__/serializer.test.ts
@@ -0,0 +1,56 @@
+import { serializeFile } from '..'
+
+const input = {
+  prefix: '\nPrefix here.\n\n<!-- lists-start -->',
+  sections: [
+    {
+      level: 1,
+      name: 'Some section',
+      items: [
+        {
+          name: 'learnxinyminutes-docs',
+          url: 'https://github.com/adambard/learnxinyminutes-docs',
+          desc: 'Code documentation written as code!',
+          extras: ['https://learnxinyminutes.com/'],
+        },
+      ],
+    },
+    {
+      level: 2,
+      name: 'Some subsection',
+      items: [
+        {
+          name: 'learnxinyminutes-docs',
+          url: 'https://github.com/adambard/learnxinyminutes-docs',
+        },
+      ],
+    },
+  ],
+  suffix: '<!-- lists-end -->\n\nSuffix here.\n',
+}
+
+const expected = `
+Prefix here.
+
+<!-- lists-start -->
+
+## Some section
+
+* [learnxinyminutes-docs](https://github.com/adambard/learnxinyminutes-docs) – Code documentation written as code!
+  - https://learnxinyminutes.com/
+
+
+### Some subsection
+
+* [learnxinyminutes-docs](https://github.com/adambard/learnxinyminutes-docs)
+
+<!-- lists-end -->
+
+Suffix here.
+`
+
+describe('.serializeFile', () => {
+  test('converts file to Markdown', () => {
+    expect(serializeFile(input)).toBe(expected)
+  })
+})
---
+++ src/serializer/index.ts
@@ -0,0 +1,21 @@
+import { ListFile, Section } from '../types'
+import { serializeItems } from './items'
+
+function serializeSection(section: Section): string {
+  const headingLevel = section.level + 1 // Always H2+
+  const heading = '#'.repeat(headingLevel) + ` ${section.name}`
+
+  return `\n${heading}\n\n${serializeItems(section.items)}\n`
+}
+
+export function serializeFile(file: ListFile): string {
+  let output = ''
+  if (file.prefix) {
+    output += `${file.prefix}\n`
+  }
+  output += file.sections.map(serializeSection).join('\n')
+  if (file.suffix) {
+    output += `\n${file.suffix}`
+  }
+  return output
+}
